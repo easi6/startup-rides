4c788d84429f950631bda71b3a990ea7cd088e08
Derek Nguyen <dikanguok@gmail.com>
2016-05-12 15:53:05 +0900

    fixed navbar on scroll

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,3 +1,23 @@
 $(function() {
   $("input[type=file]").nicefileinput({label: "+ Add pitch file"});
 });
+
+$(window).scroll(function(){
+  var topbar_height = 0;
+  // Get container scroll position
+  var fromTop = $(this).scrollTop() + topbar_height;
+  var
+    sticky_css = {
+      "position":"fixed"
+    },
+    non_sticky_css = {
+      "position":"relative"
+    }
+
+  if (fromTop > 370) {
+    $('nav').css(sticky_css);
+  } else {
+    $('nav').css(non_sticky_css);
+  }
+
+});
